780e768ffdd90bdc0503d1c430489340e040630f
feeblefruits <j.coetzee0@gmail.com>
2019-08-11 11:35:27 +0200

    Updated text-writing sections

--- scroll_main/js/main-opacity-float-script.js
+++ scroll_main/js/main-opacity-float-script.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function($){
 
 	var contentList = [
-		"Moses Mdluli (55) sits on a cushion outside. His arms crossed around his chest. He holds on tightly and coughs a couple of times. \"It's especially bad in the winter,\" he says.",
+		"Moses Mdluli sits on a cushion outside. His arms crossed around his chest. He holds on tightly and coughs a couple of times. \"It's especially bad in the winter,\" he says.",
 		"His children sit on the porch and play a game of stone-toss-and-pick-up called kudoda. \"When I see them in the mornings, I\'m alive.\", Mdluli shares.",
-		"The 55-year-old former mineworker can\’t pace or walk more than 20 metres without wheezing. He often coughs up blood in the evenings.",
+		"The 55-year-old former mineworker from the twon Hlatikulu in Eswatini can\’t pace or walk more than 20 metres without wheezing. He often coughs up blood in the evenings.",
 		"Silicosis or Worker\'s lung, as it\'s commonly known, made him redundant as a worker 14 years ago. \"They don't want skorokoro,\” he says talking about the South African gold mines.",
 		"These days he can\'t lift a bucket of water. The Swati man started as a helper and eventually worked his way up to a line manager. \“As soon as they see you coughing they chase you away.\"",
 		"Mdluli only has one of his lungs left and wheezes when he trudges. \“The one is fucked up,\” he says angrily.",
